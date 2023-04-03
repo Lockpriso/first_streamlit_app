c380e0c35f6a3136ade73a29588b58289225a11b
Lockpriso <112278941+Lockpriso@users.noreply.github.com>
2023-04-03 22:01:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,8 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+fruit_choice)
+#streamlit.dataframe(fruityvice_normalized)
 #streamlit.text(fruit_choice)
 
 
