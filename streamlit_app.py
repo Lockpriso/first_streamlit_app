1357964a0a10b5af172e213d4c1580648936703e
Lockpriso <112278941+Lockpriso@users.noreply.github.com>
2023-04-04 14:19:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,15 +66,31 @@ except URLError as e:
 #streamlit.dataframe(fruityvice_normalized)
 
 #stop everything from here
-streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#snowflake related functions
+def get_fruit_load_list()
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from FRUIT_LOAD_LIST")
+        return my_cur.fetchall()
+
+#add a button to load the fruit
+
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+    
+    
+streamlit.stop() 
+
+
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from FRUIT_LOAD_LIST")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
 
 #"Adding second selection tex
 #Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
